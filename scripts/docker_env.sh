e8f00b0bb3d75e9d48a09f518174880f63ba90bd
Kobus Goosen <kgoosen@videologyinc.com>
2022-07-21 10:15:46 +0200

    fix docker

--- scripts/docker_env.sh
+++ scripts/docker_env.sh
@@ -2,8 +2,27 @@
 export DOCKER=docker
 export DOCKER_REPO=yoedistro/yoe-build:bullseye
 
+DOCKER_IMAGE=yocto_builder
+
+SOURCED=0
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && SOURCED=1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker_build() {
+  echo "building ${SCRIPT_DIR}/Dockerfile"
+  docker build  -t "${DOCKER_IMAGE}" \
+                --build-arg "USER=$(whoami)" \
+                --build-arg "host_uid=$(id -u)" \
+                --build-arg "host_gid=$(id -g)" \
+                "${SCRIPT_DIR}"
+}
+
+check_docker() {
+  docker image inspect -f ' ' "$DOCKER_IMAGE" || docker_build
+}
+
 dkr() {
-  # check_docker || return 1
+  check_docker || return 1
 
   if [ -z "$1" ]; then
     echo "setting dkr action to shell"
@@ -12,16 +31,13 @@ dkr() {
     CMD="$@"
   fi
 
-  touch ~/.gitconfig
-  touch ~/.inputrc
-  touch ~/.yocto_history
-
-  if [ -e ~/.gitconfig ]; then
-    MAP_GITCONFIG="--volume=$HOME/.gitconfig:/home/build/.gitconfig"
-  fi
+  [ -e ~/.gitconfig ]     || touch ~/.gitconfig
+  [ -e ~/.inputrc ]       || touch ~/.inputrc
+  [ -e ~/.yocto_history ] || touch ~/.yocto_history
 
   GGID=$(id -g)
   UUID=$(id -u)
+  USER=$(whoami)
   UID_ARGS=""
   if [ "$DOCKER" = "podman" ]; then
     # https://github.com/containers/podman/issues/2180
@@ -30,16 +46,18 @@ dkr() {
 
   $DOCKER run --rm -it \
     -v "$(pwd)":"$(pwd)" \
-    -v ~/.ssh:/home/build/.ssh \
-    -v ~/.gitconfig:/home/build/.gitconfig \
-    -v ~/.inputrc:/home/build/.inputrc \
-    -v ~/.~/.yocto_history:/home/build/.bash_history \
-    $MAP_GITCONFIG \
+    -v ~/.ssh:/home/"${USER}"/.ssh \
+    -v ~/.gitconfig:/home/"${USER}"/.gitconfig \
+    -v ~/.inputrc:/home/"${USER}"/.inputrc \
+    -v ~/.yocto_history:/home/"${USER}"/.bash_history \
     -w "$(pwd)" \
+    -e "TERM=xterm-256color" \
     $UID_ARGS --user=$UUID:$GGID \
     --cpu-shares=512 \
-    ${DOCKER_REPO} /bin/bash -c "$CMD"
+    ${DOCKER_IMAGE} nice -n 10 /bin/bash -c "$CMD"
 }
+
+[ "$SOURCED" = "1" ] || dkr "$@"
 
 # bitbake() {
 #   ulimit -n 4096
