d12af3f208f1e7d67aef4e6073e1450b33f45c4b
Kobus Goosen <kgoosen@videologyinc.com>
2022-07-28 14:38:10 +0200

    reset base to fsl default

--- scripts/submod_reset.sh
+++ scripts/submod_reset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Cleans and resets a git repo and its submodules
 #https://gist.github.com/nicktoumpelis/11214362
-git reset --hard
+
+[ "$1" = "all" ] && git reset --hard
 git submodule sync --recursive
 git submodule update --init --force --recursive
-git clean -ffdx
+[ "$1" = "all" ] && git clean -ffdx
 git submodule foreach --recursive git clean -ffdx
