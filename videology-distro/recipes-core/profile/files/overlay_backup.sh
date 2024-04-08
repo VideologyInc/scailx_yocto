6ba711f3b06d37ff7f4035e4484a98c9eef16ce5
Kobus Goosen <kgoosen@videologyinc.com>
2024-04-08 11:58:45 +0200

    make ignore overlay add an ignore flag to suppress the message

--- videology-distro/recipes-core/profile/files/overlay_backup.sh
+++ videology-distro/recipes-core/profile/files/overlay_backup.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 if [[ $- == *i* ]]; then
-    if [ -d "/storage/overlay/backup" ] && [ ! -f /storage/overlay/backup_ignore ]; then 
+    if [ -d "/storage/overlay/backup" ] && [ ! -f "/storage/overlay/backup_ignore" ]; then
         echo " ~~ Update has removed User modified files from overlay ~~"
         echo "    Files moved to \"/storage/overlay/backup\". Review backup files, and copy them if necessary."
         echo "    Backup will be overwritten subsequent updates. Delete backup folder to remove this message."
@@ -11,8 +11,7 @@ if [[ $- == *i* ]]; then
             d|D ) rm -rf /storage/overlay/backup* ;;
             a|A ) tar -cf - -C /storage/overlay/backup . | tar -xf - -C / && rm -rf /storage/overlay/backup ;;
             l|L ) find /storage/overlay/backup -type f ;;
-            i|I ) touch /storage/overlay/backup_ignore ;;
-            * ) echo "";;
+            * ) echo "ignore" > "/storage/overlay/backup_ignore" ;;
         esac
     fi
 fi
