1ff05ebb9f91bf266eb2412e4364cb99f81876c3
Kobus Goosen <kgoosen@videologyinc.com>
2024-10-08 18:44:54 +0200

    chnage to ipk package management

--- scripts/sync_packages.sh
+++ scripts/sync_packages.sh
@@ -3,8 +3,5 @@
 # needs to be rnu from build folder, in yocto context.
 
 bitbake package-index
-# upload packages for all, armv8a, armv8a-mx8mp, scailx_imx8mp
-rclone sync deploy/deb/all/ scailx-storage:scailx-ppa/scailx/6.6-nanbield/deb/all/ --progress --transfers 64 --checkers 64 --checksum
-rclone sync deploy/deb/armv8a/ scailx-storage:scailx-ppa/scailx/6.6-nanbield/deb/armv8a/ --progress --transfers 64 --checkers 64 --checksum
-rclone sync deploy/deb/armv8a-mx8mp/ scailx-storage:scailx-ppa/scailx/6.6-nanbield/deb/armv8a-mx8mp --progress --transfers 64 --checkers 64 --checksum
-rclone sync deploy/deb/scailx_imx8mp/ scailx-storage:scailx-ppa/scailx/6.6-nanbield/deb/scailx_imx8mp --progress --transfers 64 --checkers 64 --checksum
+# upload packages for installable arch's 
+rclone sync deploy/ipk/ scailx-storage:scailx-ppa/scailx/6.6-scarthgap/deploy/ipk/ --progress --transfers 64 --checkers 64 --checksum
--- videology-distro/classes/scailx-ppa-uri.bbclass
+++ videology-distro/classes/scailx-ppa-uri.bbclass
@@ -1,3 +1,8 @@
+python () {
+    if 'package-management' in d.getVar('IMAGE_FEATURES').split():
+        if 'package_deb' in d.getVar('PACKAGE_CLASSES').split():
+            d.appendVar('IMAGE_PREPROCESS_COMMAND', ' do_add_scailx_ppa ')
+}
 
 do_add_scailx_ppa() {
     [ -f ${IMAGE_ROOTFS}${sysconfdir}/apt/sources.list.d/${DISTRO}.list ] && rm -f ${IMAGE_ROOTFS}${sysconfdir}/apt/sources.list.d/${DISTRO}.list
@@ -6,4 +11,3 @@ do_add_scailx_ppa() {
         echo "deb [signed-by=/etc/pki/scailx-ppa-key.pub.gpg] https://scailx-ppa.org/${DISTRO}/${DISTRO_VERSION}/deploy/deb/$f ./" >> ${IMAGE_ROOTFS}${sysconfdir}/apt/sources.list.d/${DISTRO}.list
     done
 }
-IMAGE_PREPROCESS_COMMAND += '${@bb.utils.contains("IMAGE_FEATURES", "package-management", "do_add_scailx_ppa", "", d)}'
