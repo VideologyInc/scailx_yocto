7ddd35ae9cf001465025a0460ac07682f8de5f41
PingVideology <jye@videologyinc.com>
2026-07-20 16:25:23 -0400

    AB#22571. Updated bash script to mount vdi file correctly to /mnt/nbd0.

--- my_bb/add_mount_vdi.sh
+++ my_bb/add_mount_vdi.sh
@@ -4,6 +4,10 @@ echo umount /mnt/yocto
 
 sudo umount /mnt/yocto
 
+echo add nbd service
+
+sudo modprobe nbd max_part=8
+
 echo Use qemu to add vdi file
 
 sudo qemu-nbd -c /dev/nbd0 yocto_backup.vdi
